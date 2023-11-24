05b5ee894b206278de02411ee36ba59faab628ac
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-11-24 11:06:28 +0000

    Head variable has been added

--- src/HandmanDrawing.tsx
+++ src/HandmanDrawing.tsx
@@ -1,3 +1,17 @@
+const HEAD = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  />
+);
+
 export function HangmanDrawing() {
   return (
     <div
@@ -5,6 +19,17 @@ export function HangmanDrawing() {
         position: "relative",
       }}
     >
+      <div
+        style={{
+          height: "50px",
+          width: "10px",
+          background: "black",
+          position: "absolute",
+          top: 0,
+          right: 0,
+        }}
+      />
+      {HEAD}
       <div
         style={{
           height: "10px",
