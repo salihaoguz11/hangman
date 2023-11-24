8a24e3f7a4f1650e319090d22457d66e8ca1a76a
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-11-24 11:37:19 +0000

    leftleg and right leg have been added

--- src/HandmanDrawing.tsx
+++ src/HandmanDrawing.tsx
@@ -23,6 +23,62 @@ const BODY = (
     }}
   />
 );
+const RIGHT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "150px",
+      right: "-100px",
+      rotate: "-30deg",
+      transformOrigin: "left bottom",
+    }}
+  />
+);
+const LEFT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "150px",
+      right: "10px",
+      rotate: "30deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+);
+const RIGHT_LEG = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "210px",
+      right: "-90px",
+      rotate: "60deg",
+      transformOrigin: "left bottom",
+    }}
+  />
+);
+const LEFT_LEG = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "210px",
+      right: 0,
+      rotate: "-60deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+);
 export function HangmanDrawing() {
   return (
     <div
@@ -42,6 +98,10 @@ export function HangmanDrawing() {
       />
       {HEAD}
       {BODY}
+      {RIGHT_ARM}
+      {LEFT_ARM}
+      {RIGHT_LEG}
+      {LEFT_LEG}
       <div
         style={{
           height: "10px",
